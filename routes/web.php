d61d7c4487460b81b81bc275e277c79380947ea5
Alpha <alpha.kodego@gmail.com>
2024-11-10 23:35:02 +0800

    next

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController; // Corrected line
-
-Auth::routes();
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('landing');
