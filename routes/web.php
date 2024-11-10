a594d341c3cf69cbcd594e842f138ff4127df763
Alpha <alpha.kodego@gmail.com>
2024-11-10 23:30:02 +0800

    front-end loads

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController; // Corrected line
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('landing');
 })->name('landing'); // Assigning a name to the route
 
 //Register Controller
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
 
 //Login Controller
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
